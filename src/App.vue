df2d58a6d14f68e5e6fffad5954941e47c92d786
Md. Jahangir Alam <jahangir.2012@outlook.com>
2023-07-24 03:44:19 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,150 @@
+<!-- <template>
+  <router-view />
+
+</template> -->
+<template>
+  <v-layout class="rounded rounded-md">
+    <v-system-bar>
+      <v-icon icon="mdi-wifi-strength-4"></v-icon>
+      <v-icon icon="mdi-signal" class="ms-2"></v-icon>
+      <v-icon icon="mdi-battery" class="ms-2"></v-icon>
+
+      <span class="ms-2">3:13PM</span>
+    </v-system-bar>
+    <v-app-bar class="align-center" title="VPS Admin" color="primary">
+      <v-menu>
+        <template v-slot:activator="{ props }">
+          <v-btn color="secondary" v-bind="props"> Clients </v-btn>
+        </template>
+        <v-list>
+          <v-list-item
+            v-for="(item, index) in items"
+            :key="index"
+            :value="index"
+          >
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
+      <v-btn color="secondary">
+        Orders
+
+        <v-menu activator="parent">
+          <v-list>
+            <v-list-item
+              v-for="(item, index) in items"
+              :key="index"
+              :value="index"
+            >
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-btn>
+
+      <v-btn id="menu-activator" color="secondary"> Biling </v-btn>
+
+      <v-menu activator="#menu-activator">
+        <v-list>
+          <v-list-item
+            v-for="(item, index) in items"
+            :key="index"
+            :value="index"
+          >
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+      <v-menu>
+        <template v-slot:activator="{ props }">
+          <v-btn color="secondary" v-bind="props"> Support </v-btn>
+        </template>
+        <v-list>
+          <v-list-item
+            v-for="(item, index) in items"
+            :key="index"
+            :value="index"
+          >
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+      <v-menu>
+        <template v-slot:activator="{ props }">
+          <v-btn color="secondary" v-bind="props"> Report </v-btn>
+        </template>
+        <v-list>
+          <v-list-item
+            v-for="(item, index) in items"
+            :key="index"
+            :value="index"
+          >
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+      <v-menu>
+        <template v-slot:activator="{ props }">
+          <v-btn color="secondary" v-bind="props"> Utilities </v-btn>
+        </template>
+        <v-list>
+          <v-list-item
+            v-for="(item, index) in items"
+            :key="index"
+            :value="index"
+          >
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+      <v-menu>
+        <template v-slot:activator="{ props }">
+          <v-btn color="secondary" v-bind="props"> Addins </v-btn>
+        </template>
+        <v-list>
+          <v-list-item
+            v-for="(item, index) in items"
+            :key="index"
+            :value="index"
+          >
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+      <v-spacer></v-spacer>
+
+      <v-btn variant="text" icon="mdi-magnify"></v-btn>
+
+      <v-btn variant="text" icon="mdi-filter"></v-btn>
+
+      <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer>
+      <v-list>
+        <v-list-item title="Navigation drawer"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main
+      class="d-flex align-center justify-center"
+      style="min-height: 300px"
+    >
+      Main Content
+    </v-main>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    items: [
+      { title: "Click Me" },
+      { title: "Click Me" },
+      { title: "Click Me" },
+      { title: "Click Me 2" },
+    ],
+  }),
+};
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <!-- <HelloWorld /> -->
+  <div>
+    <h1>
+      hello world
+    </h1>
+  </div>
+</template>
+
+<script setup>
+  // import HelloWorld from '@/components/HelloWorld.vue'
+</script>
